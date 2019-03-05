12f692bf3e09152802eaa66680ac6238302e7629
Wulannsarii <sariwln1706@gmail.com>
2019-03-05 14:37:16 +0700

    push pertama

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tutorial Laravel #4 : Passing Data Controller Ke View Laravel - www.lieurngoding.com</title>
+</head>
+<body>
+ 
+	<h1>Tutorial Laravel</h1>
+	<a href="https://www.lieurngoding.com/category/laravel">www.lieurngoding.com</a>
+ 
+</body>
+</html>
---
+++ resources/views/blog.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tutorial Laravel - www.lieurngoding.com</title>
+   
+</head>
+<body>
+        <h3>www.lieurngoding.com</h3>
+        <p>Seri Tutorial Laravel Lengkap Dari Dasar</p>
+        <p>Ini adalah Tulisan.</p>
+</body>
+</html>
